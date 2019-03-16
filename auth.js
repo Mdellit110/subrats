4c0c59a56d34aeefba3067c71da6b3a2d0a1aa01
matthew dellitalia <matthewdellitalia@ool-ad020328.dyn.optonline.net>
2019-03-16 15:20:33 -0400

    change secret key and move it to .env file

--- auth.js
+++ auth.js
@@ -2,22 +2,27 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const salt = 11;
-const tokenkey = 'secretsauce';
+const tokenkey = process.env.TOKENKEY;
 
+// encrypts password on register
 const hash = async (password) => {
   const digest = await bcrypt.hash(password, salt);
   return digest;
 }
 
+//generates token on login/register
 const genToken = (data) => {
   const token = jwt.sign(data, tokenkey);
   return token;
 };
 
+// used on login to verify user
 const checkPassword = async (password, password_digest) => {
   return await bcrypt.compare(password, password_digest);
 }
 
+
+// middleware used to verify user on routes
 const restrict = async (req, res, next) => {
   try {
     const token = await req.headers.authorization.split(" ")[1];

72f3a9f54d16a5bf0abc6f00ac0789b6752e43ad
Mikayda Mills <mills.ma12@gmail.com>
2019-03-19 12:32:26 -0400

    set up route to logout form

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import CommentForm from "./components/CommentForm";
 import UserProfile from "./components/UserProfile";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import LogoutForm from "./components/LogoutForm";
 // jwt-decode lets us decode json web token and access the data in them
 import decode from "jwt-decode";
 import {
@@ -209,7 +210,7 @@ class App extends Component {
       <div className="Main-app-body">
         <div className="header-container">
           <h1 className="main-title">Subway Rats</h1>
-          <Header />
+          <Header show={this.state.currentUser} />
         </div>
         <Route
           exact
@@ -287,6 +288,11 @@ class App extends Component {
           exact
           path="/station/:id/new-comment"
           render={() => <CommentForm userData={this.userData} />}
+        />
+        <Route
+          exact
+          path="/logout"
+          render={() => <LogoutForm handleLogout={this.handleLogout} />}
         />
         <Footer handleLogout={this.handleLogout} />
       </div>
